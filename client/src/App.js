8b93d8aa2ab139343341223840cbbf075d0d01d3
balayokesh <balayokeshmani@gmail.com>
2021-09-19 02:32:24 +0530

    removed router and implemented home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,42 @@
 // Import modules
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 // Import MUI packages
-import { Container, Typography } from '@material-ui/core';
+import { CssBaseline, AppBar, Container, Typography, Button, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles ((theme) => ({
+	header: {
+		padding: '1%',
+	},
+	main: {
+		
+		display: 'flex',
+		flexDirection: 'column',
+		height: '100vh',
+		justifyContent: 'center',	
+		alignItems: 'center',
+		textAlign: 'center',
+	},
+	button: {
+		margin: '1%',
+		minWidth: '50vw',
+		padding: '2%',
+	}
+}));
 
 export default function App() {
+	const classes = useStyles ();
 	return (
-		<Container>
-			<Typography>Hi</Typography>
-			<Link to='dashboard'>View submissions</Link>
-		</Container>
+		<>
+			<CssBaseline />
+			<AppBar className={classes.header}>
+				<Typography variant='h4' align='center'>Assignment submission portal</Typography>
+			</AppBar>
+
+			<Container className={classes.main}>
+				<Button variant='outlined' color='primary' className={classes.button}>View submissions</Button>
+				<Button variant='outlined' color='primary' className={classes.button}>Submit new response</Button>
+			</Container>	
+		</>
 	);
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,13 @@
 // Import packages
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, BrowserRouter } from 'react-router-dom';
 
 // Import files
 import App from './App';
-import Dashboard from './Dashboard';
-
-const Routing = () => {
-  return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Route path='/' exact component={App} />
-      <Route path='/dashboard' exact component={Dashboard} />
-    </BrowserRouter>
-  )
-}
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routing />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
